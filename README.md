1f5fd83c2057a9b3edac23b93f80f29604387ecd
Henry Hirsch <2451426+hhirsch@users.noreply.github.com>
2025-08-18 15:04:26 +0200

    feat: add minimal deno stack

---
+++ README.md
@@ -0,0 +1,14 @@
+# Dino Chat
+A chat server in deno.
+
+[Download](https://nixos.org/download/) and Install Nix the package manager.
+
+Setup your environment for deno with
+```
+nix-shell
+```
+
+Run the project with
+```
+deno run main.ts
+```
---
+++ main.ts
@@ -0,0 +1,28 @@
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+
+const messages = new Map<string, any>();
+messages.set("1", {
+    id: "1",
+    content: "Hello, how do you do?",
+    author: "Bob",
+});
+
+const router = new Router();
+router
+    .get("/", (context) => {
+        context.response.body = "Hello world!";
+    })
+    .get("/message", (context) => {
+        context.response.body = Array.from(messages.values());
+    })
+    .get("/message/:id", (context) => {
+        if (messages.has(context?.params?.id)) {
+            context.response.body = messages.get(context.params.id);
+        }
+    });
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+await app.listen({ port: 8000 });
