d70cddc51e1767bd1e419ba51bcb6b39b1cb7235
Henry Hirsch <2451426+hhirsch@users.noreply.github.com>
2025-08-20 09:19:15 +0200

    feat: successful round trip to postgreSQL

--- main.ts
+++ main.ts
@@ -1,37 +1,43 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
-
-const messages = new Map<string, any>();
-messages.set(
-    "1",
-    {
-        id: "1",
-        content: "Hello, how do you do?",
-        author: "Bob",
-    },
-    "2",
-    {
-        id: "2",
-        content: "Are you there",
-        author: "Bob",
-    },
-);
+import { Client } from "jsr:@db/postgres";
+import * as os from "node:os";
+const userInfo = os.userInfo();
+const client = new Client({
+    database: "dino",
+    hostname: "./",
+    host_type: "socket",
+    port: 5432,
+    user: userInfo.username,
+});
 
 const router = new Router();
 router
-    .get("/", (context) => {
-        context.response.body = "Hello world!";
-    })
-    .get("/message", (context) => {
-        context.response.body = Array.from(messages.values());
-    })
-    .get("/message/:id", (context) => {
-        if (messages.has(context?.params?.id)) {
-            context.response.body = messages.get(context.params.id);
+    .get("/test", async (context) => {
+        try {
+            await client.connect();
+            const name = "Charlie";
+            const sql = "SELECT * FROM messages WHERE receiver = $1";
+            const res = await client.queryObject(sql, [name]);
+            context.response.body = res.rows;
+        } catch (err) {
+            console.error("DB connection failed:", err.message);
+            console.log(err.message);
+            console.error(err);
+        } finally {
+            try {
+                await client.end();
+            } catch (err) {
+                console.log("Error closing client: " + err.message);
+            }
         }
     });
+try {
+    const app = new Application();
+    app.use(router.routes());
+    app.use(router.allowedMethods());
 
-const app = new Application();
-app.use(router.routes());
-app.use(router.allowedMethods());
-
-await app.listen({ port: 8000 });
+    await app.listen({ port: 8000 });
+    await client.end();
+} catch (err) {
+    console.log("Uncaught error: " + err.message);
+}
--- shell.nix
+++ shell.nix
@@ -14,10 +14,12 @@ pkgs.mkShell {
       echo -e "To initialize postgreSQL use \e[1minitDatabase\e[0m and run the database with \e[1mrunDatabase\e[0m."
       echo -e "Create the database with \e[1mcreateDatabase\e[0m."
     fi
-    echo -e "When you are done, stop postgreSQL with \e[1mstopDatabase\e[0m."  
+    echo -e "When you are done, stop postgreSQL with \e[1mstopDatabase\e[0m."
+    echo -e "Type \e[1mrun\e[0m to run the application."  
     alias initDatabase='initdb -D .data/postgresql'
     alias runDatabase='pg_ctl -D .data/postgresql -l logfile -o "--unix_socket_directories=$PWD" start'
     alias createDatabase='createdb -h $PWD dino'
     alias stopDatabase='pg_ctl -D .data/postgresql stop'
+    alias run='deno run --allow-net main.ts'
   '';
 }
